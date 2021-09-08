d37fda796736b1cac17d6627ab921b7411b7becb
max <max@swk-web.com>
2021-09-08 10:06:08 +0200

    fix: Remove leading spaces of the generated robots.txt

---
+++ src/Core/Framework/Adapter/Twig/Extension/LeadingSpacesFilterExtension.php
@@ -0,0 +1,21 @@
+<?php declare(strict_types=1);
+
+namespace Frosh\RobotsTxt\Core\Framework\Adapter\Twig\Extension;
+
+use Twig\Extension\AbstractExtension;
+use Twig\TwigFilter;
+
+class LeadingSpacesFilterExtension extends AbstractExtension
+{
+    public function getFilters(): array
+    {
+        return [
+            new TwigFilter('remove_leading_spaces', [$this, 'removeLeadingSpaces'], ['is_safe' => ['all']]),
+        ];
+    }
+
+    public function removeLeadingSpaces(string $content): string
+    {
+        return trim(preg_replace('/^ +/m', '', $content));
+    }
+}
